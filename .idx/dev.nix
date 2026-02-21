b1d1d7083ae9209c492afa1380fb0830c3732bc2
bkjyk07 <bkjyk07@gmail.com>
2026-02-21 06:10:14 +0000

    Update project files

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [
+  packages = [ pkgs.gh 
     pkgs.nodejs_20
     pkgs.python3
   ];
